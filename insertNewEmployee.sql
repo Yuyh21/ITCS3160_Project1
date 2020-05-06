d05863085437c274143c622f9ff16d701ccebdd7
ni121498 <32403702+ni121498@users.noreply.github.com>
2020-05-06 17:16:45 -0400

    Add files via upload

---
+++ insertNewEmployee.sql
@@ -0,0 +1,30 @@
+DELIMITER //
+
+CREATE TRIGGER insertNewEmployee
+AFTER INSERT ON employees
+FOR EACH ROW
+BEGIN 
+	IF new.employeeTypeId = 1 THEN
+		INSERT INTO faculty_employee (employeeTypeId, employeeId)
+		values (new.employeeTypeId,  new.employeeId);
+        
+	ELSEIF new.employeeTypeId = 2 THEN
+		INSERT INTO staff_employee (employeeTypeId, employeeId)
+        values (new.employeeTypeId, new.employeeId);
+	
+    ELSE
+		INSERT INTO student_employee (employeeTypeId, employeeId)
+        values (new.employeeTypeId, new.employeeId);
+        
+        INSERT INTO delivery_personnel (employeeTypeId, employeeId)
+        values (new.employeeTypeId, new.employeeId);
+        
+        END IF;
+END;
+	
+DELIMITER ;
+
+
+
+
+  
